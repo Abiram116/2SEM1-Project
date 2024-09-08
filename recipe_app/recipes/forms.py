a29a7b845b8f05f8e5162ddb23cd7864325d7347
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2024-09-08 21:55:34 +0530

    improved create recipie page

--- recipe_app/recipes/forms.py
+++ recipe_app/recipes/forms.py
@@ -4,8 +4,13 @@ from .models import Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['name', 'description', 'image', 'rating','date', 'is_inspiring','ingredients']
+        fields = ['name', 'description', 'image', 'rating', 'date', 'is_inspiring', 'ingredients']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'ingredients': forms.Textarea(attrs={'rows': 5}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control recipe-textarea', 'rows': 5}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'is_inspiring': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'ingredients': forms.Textarea(attrs={'class': 'form-control recipe-textarea', 'rows': 5}),
         }
